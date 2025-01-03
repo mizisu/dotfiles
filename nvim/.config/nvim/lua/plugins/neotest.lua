333982be794897b671c8f2c91d4272460c544e88
mizisu <alwltn960406@gmail.com>
2025-01-03 16:10:13 +0900

    Update nvim setup

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -7,6 +7,8 @@ return {
   opts = {
     adapters = {
       ["neotest-python"] = {
+        env = { DJANGO_SETTINGS_MODULE = "server.settings.test" },
+        django_settings_module = "server.settings.test",
         args = { "--settings=server.settings.test", "--keepdb" },
       },
     },
