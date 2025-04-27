927eb15887c371c92aea96f47c8608e0f3d44fe3
mizisu <alwltn960406@gmail.com>
2025-04-27 21:20:08 +0900

    Update nvim

--- nvim/.config/nvim/README.md
+++ nvim/.config/nvim/README.md
@@ -7,3 +7,4 @@
 - 서버 실행시 줄바꿈 하지 않도록
 - Scratch pad
 - Octo plugin 으로 PR diff 확인하기
+- Summary에서 TEST 동작할때는 VIEW_TRACEBACK=false로 동작하게 하기
--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -10,25 +10,78 @@ return {
       "nvim-neotest/neotest-plenary",
     },
     keys = {
-      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run the nearest test" },
-      { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug the nearest test" },
-      { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run the current file" },
-      { "<leader>tA", function() require("neotest").run.run({ suite = true }) end, desc = "Run all tests" },
+      {
+        "<leader>tr",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run the nearest test",
+      },
+      {
+        "<leader>td",
+        function()
+          require("neotest").run.run({ strategy = "dap" })
+        end,
+        desc = "Debug the nearest test",
+      },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run the current file",
+      },
+      {
+        "<leader>tA",
+        function()
+          require("neotest").run.run({ suite = true })
+        end,
+        desc = "Run all tests",
+      },
 
-      { "<leader>ta", function() require("neotest").run.attach() end, desc = "Attach to the nearest test" },
+      {
+        "<leader>ta",
+        function()
+          require("neotest").run.attach()
+        end,
+        desc = "Attach to the nearest test",
+      },
 
-      { "<leader>tu", function() require("neotest").run.stop() end, desc = "Stop the test" },
-      
-      { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Show test ouput" },
-      { "<leader>tp", function() require("neotest").output_panel.toggle() end, desc = "Toggle test output panel" },
+      {
+        "<leader>tu",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop the test",
+      },
 
-      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Show test summary" },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ enter = true })
+        end,
+        desc = "Show test ouput",
+      },
+      {
+        "<leader>tp",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle test output panel",
+      },
 
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Show test summary",
+      },
     },
     config = function()
       -- Django 환경 변수 미리 설정
       vim.env.DJANGO_SETTINGS_MODULE = "server.settings.test"
-      
+
       require("neotest").setup({
         adapters = {
           require("neotest-python")({
@@ -39,6 +92,9 @@ return {
             },
           }),
           require("neotest-plenary"),
+        },
+        output = {
+          open_on_run = true,
         },
       })
     end,
