7b2d81af2a2fe682c65390de2998e781af9fdde3
mizisu <alwltn960406@gmail.com>
2025-08-06 17:05:23 +0900

    Update nvim plugins

--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,13 +1,31 @@
+-- return {
+--     "linux-cultist/venv-selector.nvim",
+--     dependencies = {
+--         "neovim/nvim-lspconfig",
+--         "nvim-telescope/telescope.nvim",
+--     },
+--     lazy = false,
+--     branch = "regexp", -- regexp 브랜치 사용
+--     config = function()
+--         require("venv-selector").setup()
+--     end,
+--     keys = {{"<leader>cv", "<cmd>VenvSelect<cr>"}}
+-- }
 return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "nvim-telescope/telescope.nvim",
-    },
-    lazy = false,
-    branch = "regexp", -- regexp 브랜치 사용
-    config = function()
-        require("venv-selector").setup()
-    end,
-    keys = {{"<leader>cv", "<cmd>VenvSelect<cr>"}}
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+    { "<leader>cv", "<cmd>VenvSelect<cr>" },
+  },
+  ---@type venv-selector.Config
+  opts = {
+    -- Your settings go here
+  },
 }
