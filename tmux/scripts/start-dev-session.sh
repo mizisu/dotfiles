9ae4a7ce682e343fd15217f13defe0099a093ecd
Charles Kim <alwltn960406@gmail.com>
2026-04-24 14:43:29 +0900

    chore: update configuration

--- tmux/scripts/start-dev-session.sh
+++ tmux/scripts/start-dev-session.sh
@@ -27,10 +27,12 @@ app_dir="$launch_root/app"
 printf -v left_cmd 'cd %q && uv run manage.py runserver 0.0.0.0:7777 --settings=server.settings.local --skip-checks' "$launch_root"
 printf -v right_cmd 'cd %q && pnpm start' "$app_dir"
 
-tmux new-session -d -s "$session_name" -n "$window_name" "$left_cmd"
-tmux split-window -h -t "${session_name}:${window_name}" "$right_cmd"
-tmux set-option -t "$session_name" remain-on-exit on
+left_pane="$(tmux new-session -d -P -F '#{pane_id}' -s "$session_name" -n "$window_name" -c "$launch_root")"
+right_pane="$(tmux split-window -h -P -F '#{pane_id}' -t "${session_name}:${window_name}" -c "$launch_root")"
+
+tmux send-keys -t "$left_pane" "$left_cmd" C-m
+tmux send-keys -t "$right_pane" "$right_cmd" C-m
 
 tmux select-layout -t "${session_name}:${window_name}" even-horizontal >/dev/null
-tmux select-pane -t "${session_name}:${window_name}.1"
+tmux select-pane -t "$left_pane"
 tmux switch-client -t "$session_name"
